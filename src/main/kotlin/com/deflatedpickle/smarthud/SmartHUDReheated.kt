0560353b13915a027f156fe723bc24fdf1b83e09
DeflatedPickle <deflatedpickle@gmail.com>
2022-01-04 21:31:41 +0000

    Embedded Kotlin for scripting

--- src/main/kotlin/com/deflatedpickle/smarthud/SmartHUDReheated.kt
+++ src/main/kotlin/com/deflatedpickle/smarthud/SmartHUDReheated.kt
@@ -7,24 +7,26 @@ package com.deflatedpickle.smarthud
 import com.deflatedpickle.smarthud.api.Alignment
 import com.deflatedpickle.smarthud.api.Inventory
 import com.deflatedpickle.smarthud.api.Orientation
-import com.deflatedpickle.smarthud.api.Towards
-import com.deflatedpickle.smarthud.impl.Dodge
-import com.deflatedpickle.smarthud.impl.Position
 import com.deflatedpickle.smarthud.impl.Section
 import net.fabricmc.api.ClientModInitializer
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.hud.InGameHud
 import net.minecraft.client.util.math.MatrixStack
-import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.ArmorItem
-import net.minecraft.item.ArrowItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import net.minecraft.item.Items
 import net.minecraft.util.Arm
-import net.minecraft.util.Hand
 import net.minecraft.util.collection.DefaultedList
+import java.io.File
+import kotlin.script.experimental.api.ResultValue
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.ScriptCompilationConfiguration
+import kotlin.script.experimental.api.ScriptEvaluationConfiguration
+import kotlin.script.experimental.api.defaultImports
+import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
+import kotlin.script.experimental.jvm.jvm
+import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
 
 @Suppress("UNUSED")
 object SmartHUDReheated : ClientModInitializer {
@@ -34,82 +36,10 @@ object SmartHUDReheated : ClientModInitializer {
     private const val AUTHOR = "$[author]"
     private const val VERSION = "$[version]"
 
-    private const val DISTANCE = 7
-
-    private const val POSITION = "pos"
-    private const val ORIENTATION = "or"
-    private const val OFFSET = "off"
-    private const val ITEMS = "i"
-
+    const val DISTANCE = 7
     const val SIZE = 22
 
-    private val sections = listOf(
-        // Navigation/Time
-        Section(
-            position = Position(
-                vertical = Alignment.END,
-            ),
-            offset = Pair(182 / 2 + SIZE / 2 + 7, 0),
-            items = listOf(
-                { it.item == Items.CLOCK },
-                { it.item == Items.COMPASS },
-            )
-        ),
-        // Rockets
-        Section(
-            position = Position(
-                vertical = Alignment.END,
-            ),
-            offset = Pair(-182 / 2 - SIZE / 2 - 7, 0),
-            items = listOf { it.item == Items.FIREWORK_ROCKET },
-            dodge = Dodge(
-                upon = {
-                    it.getStackInHand(Hand.OFF_HAND).item != Items.AIR
-                },
-                offset = Pair(-SIZE + 1, 0),
-            ),
-            towards = Towards.LEFT,
-        ),
-        // Arrows
-        Section(
-            position = Position(
-                vertical = Alignment.END,
-            ),
-            offset = Pair(-182 / 2 - SIZE / 2 - 7, 0),
-            items = listOf { it.item is ArrowItem },
-            dodge = Dodge(
-                upon = { player ->
-                    player.getStackInHand(Hand.OFF_HAND).item != Items.AIR ||
-                        player.inventory.main.any { it.item == Items.FIREWORK_ROCKET }
-                },
-                offset = Pair(0, -SIZE + 1),
-            ),
-            towards = Towards.LEFT,
-            limit = -1,
-        ),
-        // Armour
-        Section(
-            position = Position(
-                vertical = Alignment.END,
-            ),
-            offset = Pair(182 / 2 + SIZE / 2 + 7, 0),
-            items = listOf(
-                { it.item is ArmorItem && (it.item as ArmorItem).slotType == EquipmentSlot.HEAD },
-                { it.item == Items.ELYTRA || it.item is ArmorItem && (it.item as ArmorItem).slotType == EquipmentSlot.CHEST },
-                { it.item is ArmorItem && (it.item as ArmorItem).slotType == EquipmentSlot.LEGS },
-                { it.item is ArmorItem && (it.item as ArmorItem).slotType == EquipmentSlot.FEET },
-            ),
-            dodge = Dodge(
-                upon = { player ->
-                    player.inventory.main.any { it.item == Items.CLOCK || it.item == Items.COMPASS }
-                },
-                offset = Pair(0, -SIZE + 1),
-            ),
-            inventories = listOf(
-                Inventory.ARMOUR,
-            ),
-        ),
-    )
+    private lateinit var sections: List<Section>
 
     var enabled = true
 
@@ -117,6 +47,40 @@ object SmartHUDReheated : ClientModInitializer {
         println(listOf(MOD_ID, NAME, GROUP, AUTHOR, VERSION))
 
         KeyboardHandler.initialize()
+
+        val file = File("${System.getProperty("user.dir")}/config/smarthud.kts")
+
+        val cc = ScriptCompilationConfiguration {
+            jvm {
+                dependenciesFromCurrentContext(
+                    wholeClasspath = true
+                )
+            }
+            defaultImports(
+                "com.deflatedpickle.smarthud.SmartHUDReheated.SIZE",
+                "com.deflatedpickle.smarthud.api.*",
+                "com.deflatedpickle.smarthud.impl.*",
+            )
+        }
+
+        val ec = ScriptEvaluationConfiguration {
+        }
+
+        val eval = BasicJvmScriptingHost().eval(file.toScriptSource(), cc, ec)
+
+        when (eval) {
+            is ResultWithDiagnostics.Success -> {
+                when (val result = eval.value.returnValue) {
+                    is ResultValue.Value -> {
+                        sections = result.value as List<Section>
+                    }
+                    is ResultValue.Unit -> TODO()
+                    is ResultValue.Error -> TODO()
+                    ResultValue.NotEvaluated -> TODO()
+                }
+            }
+            is ResultWithDiagnostics.Failure -> {}
+        }
     }
 
     fun drawSection(inGameHud: InGameHud, drawFunc: (Int, Int, Int, ItemStack) -> Unit) {
